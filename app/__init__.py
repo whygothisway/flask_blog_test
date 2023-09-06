9c477610e214feb0c150ea4d8446ef98f8cf8d0f
JS <thisisjiewen@gmail.com>
2023-09-06 22:51:30 +0800

    Ensure that the necessary database tables are created

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import routes
+
+from .models import User, Post
+
+from .models import User, Post
+
+# ensure that the necessary database tables are created within the Flask application context
+with app.app_context():
+    db.create_all()
